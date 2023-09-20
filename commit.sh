4436867dd5587d783806f3663ba001c0c16174d3
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2023-09-20 13:09:07 -0500

    update

---
+++ commit.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+set -euxo nounset
+(( $UID ))
+if (( ! $# )) ; then
+  M=update
+else M="$*"
+fi
+git add .
+git commit -m "$M"
+git push --set-upstream origin HEAD:kali
+cd  ..
+git add .
+git commit -m "$M"
+git push
+
